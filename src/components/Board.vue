97c9d36efd4fe6cf5792ca719c9df1efe88e57ad
dhythm <yuta.okada.20@gmail.com>
2021-08-28 10:49:24 +0900

    feat: work for 4-3

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -4,7 +4,7 @@
       my Trello
     </header>
     <main>
-      <p class="info-line">All: 0 tasks</p>
+      <p class="info-line">All: {{ totalCardCount }} tasks</p>
       <div class="list-index">
         <list
           v-for="(item, index) in lists"
@@ -31,6 +31,9 @@ export default {
   },
   computed: {
     ...mapState(["lists"]),
+    totalCardCount() {
+      return this.$store.getters.totalCardCount;
+    },
   },
 };
 </script>
